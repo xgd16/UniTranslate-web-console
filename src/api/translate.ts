dfb301adbe44103ba40148641e442097dc22dd64
x <895740313@qq.com>
2024-04-30 11:55:50 +0800

    Upgrade: Optimized layout Upgrade: Compatible with batch translation

--- src/api/translate.ts
+++ src/api/translate.ts
@@ -1,66 +1,75 @@
-import type {Response} from "@/lib/request";
+import type { Response } from "@/lib/request";
 import req from "@/lib/request";
 
 import type {
-    BaiduConfig,
-    ChatGPTConfig,
-    ConfigList,
-    DeeplConfig,
-    GoogleConfig,
-    HuoShanConfig,
-    RequestRecord,
-    TencentConfig,
-    Translate,
-    XunFeiConfig,
-    YouDaoConfig
+  BaiduConfig,
+  ChatGPTConfig,
+  ConfigList,
+  DeeplConfig,
+  GoogleConfig,
+  HuoShanConfig,
+  RequestRecord,
+  TencentConfig,
+  Translate,
+  XunFeiConfig,
+  YouDaoConfig,
 } from "@/types/props";
 
-
 export const translateRequest = (
-    data: {from: string, to: string, text: string, platform: string} = {
-        from: 'auto',
-        to: 'en',
-        text: '',
-        platform: '',
-    }
+  data: {
+    from: string;
+    to: string;
+    text: string[];
+    platform: string;
+  } = {
+    from: "auto",
+    to: "en",
+    text: [],
+    platform: "",
+  }
 ) => {
-    return req<Response<Translate>>({
-        url: '/api/translate',
-        method: 'POST',
-        data: data
-    })
-}
+  return req<Response<Translate>>({
+    url: "/api/translate",
+    method: "POST",
+    data: data,
+  });
+};
 
-export const addConfigRequest = (
-    data: {
-        platform: string,
-        status: boolean,
-        level: number,
-        cfg: BaiduConfig|YouDaoConfig|GoogleConfig|DeeplConfig|ChatGPTConfig|XunFeiConfig|TencentConfig|HuoShanConfig
-        "type": string,
-    }
-) => {
-    return req<Response<string[]>>({
-        url: '/api/addConfig',
-        method: 'POST',
-        data: data
-    })
-}
+export const addConfigRequest = (data: {
+  platform: string;
+  status: boolean;
+  level: number;
+  cfg:
+    | BaiduConfig
+    | YouDaoConfig
+    | GoogleConfig
+    | DeeplConfig
+    | ChatGPTConfig
+    | XunFeiConfig
+    | TencentConfig
+    | HuoShanConfig;
+  type: string;
+}) => {
+  return req<Response<string[]>>({
+    url: "/api/addConfig",
+    method: "POST",
+    data: data,
+  });
+};
 
 export const getConfigList = () => {
-    return req<Response<ConfigList[]>>({
-        url: '/api/getConfigList',
-        method: 'GET'
-    })
-}
-
+  return req<Response<ConfigList[]>>({
+    url: "/api/getConfigList",
+    method: "GET",
+  });
+};
 
 export const getRequestRecord = (
-    params: {page: number, size: number} = {page: 1, size: 10}
+  params: { page: number; size: number } = { page: 1, size: 10 }
 ) => {
-    return req<Response<RequestRecord>>({
-        url: '/api/getRequestRecord',
-        method: 'GET',
-        params
-    })
-}
+  return req<Response<RequestRecord>>({
+    url: "/api/getRequestRecord",
+    method: "GET",
+    params,
+  });
+};
--- src/views/TranslateView.vue
+++ src/views/TranslateView.vue
@@ -1,176 +1,190 @@
 <template>
-  <el-form v-loading="loading" :model="form" class="el-col-12" label-width="120px" label-position="top">
-    <el-form-item label="源文本语言">
-      <el-select-v2
+  <el-form
+    v-loading="loading"
+    :model="form"
+    class="el-col-12"
+    label-width="120px"
+    label-position="top"
+  >
+    <el-row>
+      <el-form-item label="源文本语言" class="el-col-sm-6">
+        <el-select-v2
           v-model="form.fromLang"
           filterable
           :options="fromOptions"
           placeholder="Please select"
           style="width: 240px; margin-right: 16px; vertical-align: middle"
-      />
-    </el-form-item>
-    <el-form-item label="目标语言">
-      <el-select-v2
+        />
+      </el-form-item>
+      <el-form-item label="目标语言" class="el-col-sm-6">
+        <el-select-v2
           v-model="form.toLang"
           filterable
           :options="options"
           placeholder="Please select"
           style="width: 240px; margin-right: 16px; vertical-align: middle"
-      />
-    </el-form-item>
-    <el-form-item label="平台">
-      <el-select-v2
+        />
+      </el-form-item>
+      <el-form-item label="平台" class="el-col-sm-6">
+        <el-select-v2
           v-model="form.platform"
           filterable
           :options="platformOptions"
           placeholder="Please select"
           style="width: 240px; margin-right: 16px; vertical-align: middle"
-      />
-    </el-form-item>
+        />
+      </el-form-item>
+    </el-row>
     <el-form-item label="需要翻译的内容">
-      <el-input type="textarea" v-model="form.text" :autosize="{ minRows: 30, maxRows: 30 }"></el-input>
+      <el-input
+        type="textarea"
+        v-model="form.text"
+        :autosize="{ minRows: 30, maxRows: 30 }"
+      ></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" plain @click="submitTranslate">翻译</el-button>
     </el-form-item>
   </el-form>
   <div class="el-col-12" style="padding-left: 15px">
-    <textarea style="
-      background-color: var(--el-color-black);
-      resize: none;
-      border-radius: 4px;
-      border: none;
-      box-shadow: 0 0 0 1px var(--el-input-border-color,var(--el-border-color)) inset;
-      padding: 5px 11px;
-      width: 100%;
-      height: 100%;
-    ">{{ translateBody }}</textarea>
+    <textarea
+      style="
+        background-color: var(--el-color-black);
+        resize: none;
+        border-radius: 4px;
+        border: none;
+        box-shadow: 0 0 0 1px
+          var(--el-input-border-color, var(--el-border-color)) inset;
+        padding: 5px 11px;
+        width: 100%;
+        height: 100%;
+      "
+      >{{ translateBody }}</textarea
+    >
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from "vue";
+import { langArr, langMap } from "@/lib/common";
+import { translateRequest } from "@/api/translate";
+import { ElMessage } from "element-plus";
 
-import {reactive, ref} from "vue";
-import {langArr, langMap} from "@/lib/common";
-import {translateRequest} from "@/api/translate";
-import {ElMessage} from "element-plus";
-
-const loading = ref(false)
+const loading = ref(false);
 
 type selectOptionType = {
-  value: string,
-  label: string,
-  disabled: boolean
-}[]
+  value: string;
+  label: string;
+  disabled: boolean;
+}[];
 
-let options: selectOptionType  = []
+let options: selectOptionType = [];
 
 for (const k in langArr) {
-  const item = langArr[k]
+  const item = langArr[k];
   options.push({
     value: item,
     label: `${item} ${langMap[item]}`,
     disabled: false,
-  })
+  });
 }
 
-let fromOptions: selectOptionType = [...options]
+let fromOptions: selectOptionType = [...options];
 fromOptions.unshift({
-  value: 'auto',
-  label: 'auto 自动识别',
-  disabled: false
-})
+  value: "auto",
+  label: "auto 自动识别",
+  disabled: false,
+});
 
 let platformOptions: selectOptionType = [
   {
-    value: '',
-    label: 'auto 自动',
-    disabled: false
+    value: "",
+    label: "auto 自动",
+    disabled: false,
   },
   {
-    value: 'Baidu',
-    label: 'Baidu 百度',
-    disabled: false
+    value: "Baidu",
+    label: "Baidu 百度",
+    disabled: false,
   },
   {
-    value: 'YouDao',
-    label: 'YouDao 有道',
-    disabled: false
+    value: "YouDao",
+    label: "YouDao 有道",
+    disabled: false,
   },
   {
-    value: 'Google',
-    label: 'Google 谷歌',
-    disabled: false
+    value: "Google",
+    label: "Google 谷歌",
+    disabled: false,
   },
   {
-    value: 'Deepl',
-    label: 'Deepl',
-    disabled: false
+    value: "Deepl",
+    label: "Deepl",
+    disabled: false,
   },
   {
-    value: 'ChatGPT',
-    label: 'ChatGPT',
-    disabled: false
+    value: "ChatGPT",
+    label: "ChatGPT",
+    disabled: false,
   },
   {
-    value: 'XunFei',
-    label: 'XunFei',
-    disabled: false
+    value: "XunFei",
+    label: "XunFei",
+    disabled: false,
   },
   {
-    value: 'XunFeiNiu',
-    label: 'XunFei (niutrans)',
-    disabled: false
+    value: "XunFeiNiu",
+    label: "XunFei (niutrans)",
+    disabled: false,
   },
   {
-    value: 'Tencent',
-    label: 'Tencent 腾讯',
-    disabled: false
+    value: "Tencent",
+    label: "Tencent 腾讯",
+    disabled: false,
   },
   {
     value: "HuoShan",
     label: "HuoShan 字节跳动",
-    disabled: false
+    disabled: false,
   },
   {
     value: "PaPaGo",
     label: "PaPaGo",
-    disabled: false
-  }
-]
-
+    disabled: false,
+  },
+];
 
 const form = reactive({
-  fromLang: 'auto',
-  toLang: 'en',
-  text: '',
-  platform: ''
-})
+  fromLang: "auto",
+  toLang: "en",
+  text: "",
+  platform: "",
+  batch: false,
+});
 
-const translateBody = ref()
+const translateBody = ref();
 
 const submitTranslate = () => {
   if (!form.text) {
-    ElMessage.warning("请填写需要翻译的内容...")
-    return
+    ElMessage.warning("请填写需要翻译的内容...");
+    return;
   }
-  loading.value = true
+  loading.value = true;
   translateRequest({
     from: form.fromLang,
     to: form.toLang,
-    text: form.text,
+    text: form.text.split("\n"),
     platform: form.platform,
-  }).then((res) => {
-    loading.value = false
-    if (res.code != 1000) return;
-    translateBody.value = JSON.stringify(res.data, null, 2)
-  }).catch(() => {
-    loading.value = false
   })
-}
-
+    .then((res) => {
+      loading.value = false;
+      if (res.code != 1000) return;
+      translateBody.value = JSON.stringify(res.data, null, 2);
+    })
+    .catch(() => {
+      loading.value = false;
+    });
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
